f8e373943fc535f24dd6e8cc50fd67cd6430b519
RussGravy19 <94469528+RussGravy19@users.noreply.github.com>
2021-11-30 11:00:20 -0700

    missing master slave func
    almost all other outputs (welcome console, stepper motor, inter-board communication) are working.

--- master/TIM.c
+++ master/TIM.c
@@ -56,12 +56,6 @@ void Timer_Init(void) {
 	GPIOB->MODER &= 0xFFFFFF55; //motor
 	GPIOB->MODER |= 0x00000055;
 	
-	//GPIOC->MODER &= 0x3FFFFFFF; //buzzer
-	//GPIOC->MODER |= 0x40000000;
-	
-	//GPIOC->MODER &= 0xF3FFFFFF; // buzzer on C13
-	//GPIOC->MODER |= 0x04000000;
-	
 	GPIOC->MODER &= 0xFFFFFCFF; // buzzer on C4
 	GPIOC->MODER |= 0x00000100;
 	
@@ -123,12 +117,8 @@ void buzz_delay(void) { // debounce and delay
 void buzz(void) {
 	volatile unsigned int t = 1600;
 	while (t != 0) {
-		//GPIOC->ODR &= 0xFFFF7FFF;
-		//GPIOC->ODR &= 0xFFFFDFFF;
 		GPIOC->ODR &= 0xFFFFFFEF;
 		buzz_delay();
-		//GPIOC->ODR |= 0x00002000;
-		//GPIOC->ODR |= 0x00008000;
 		GPIOC->ODR |= 0x00000010;
 		buzz_delay();
 		t--;
@@ -136,7 +126,7 @@ void buzz(void) {
 }
 
 void wind_delay(void) { // debounce and delay
-	volatile unsigned int t = 500;
+	volatile unsigned int t = 3000;
 	while (t != 0) {
 		t -= 1;
 	}
@@ -193,65 +183,6 @@ void wind(unsigned int secs) {
 		ticks--;
 		wind_delay();
 		if (ticks == 0) break;
-		
-		/*switch (step) {
-			case 0: 
-				GPIOB->ODR &= 0xFFFFFFF1; //A
-				GPIOB->ODR |= 0x00000001; //A
-				step = 7;
-				ticks--;
-				wind_delay();
-				break;
-			case 1: 
-				GPIOB->ODR &= 0xFFFFFFF3; //AB
-				GPIOB->ODR |= 0x00000003; //AB
-				step = 6;
-				ticks--;
-				wind_delay();
-				break;
-			case 2: 
-				GPIOB->ODR &= 0xFFFFFFF2; //B
-				GPIOB->ODR |= 0x00000002; //B
-				step = 5;
-				ticks--;
-				wind_delay();
-				break;
-			case 3: 
-				GPIOB->ODR &= 0xFFFFFFF6; //BC
-				GPIOB->ODR |= 0x00000006; //BC
-				step = 4;
-				ticks--;
-				wind_delay();
-				break;
-			case 4: 
-				GPIOB->ODR &= 0xFFFFFFF4; //C
-				GPIOB->ODR |= 0x00000004; //C
-				step = 3;
-				ticks--;
-				wind_delay();
-				break;
-			case 5: 
-				GPIOB->ODR &= 0xFFFFFFFC; //CD
-				GPIOB->ODR |= 0x0000000C; //CD
-				step = 2;
-				ticks--;
-				wind_delay();
-				break;
-			case 6: 
-				GPIOB->ODR &= 0xFFFFFFF8; //D
-				GPIOB->ODR |= 0x00000008; //D
-				step = 1;
-				ticks--;
-				wind_delay();
-				break;
-			case 7: 
-				GPIOB->ODR &= 0xFFFFFFF9; //DA
-				GPIOB->ODR |= 0x00000009; //DA
-				step = 0;
-				ticks--;
-				wind_delay();
-				break;
-		}*/
 	}
 }
 
